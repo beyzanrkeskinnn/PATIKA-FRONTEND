23f6d64e1fcddeb52161c18c71afba9f38aae11d
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-07-20 11:57:40 +0300

    update to sound links

--- Hafta-6/Odev_2_DrumKit/js/script.js
+++ Hafta-6/Odev_2_DrumKit/js/script.js
@@ -3,47 +3,47 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       id: "button1",
       key: "a",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/boom.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/boom.wav",
     },
     {
       id: "button2",
       key: "s",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/clap.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/clap.wav",
     },
     {
       id: "button3",
       key: "d",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/hihat.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/hihat.wav",
     },
     {
       id: "button4",
       key: "f",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/kick.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/kick.wav",
     },
     {
       id: "button5",
       key: "g",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/openhat.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/openhat.wav",
     },
     {
       id: "button6",
       key: "h",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/ride.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/ride.wav",
     },
     {
       id: "button7",
       key: "j",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/snare.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/snare.wav",
     },
     {
       id: "button8",
       key: "k",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/tink.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/tink.wav",
     },
     {
       id: "button9",
       key: "l",
-      audio: "/Hafta-6/Odev_2_DrumKit/assests/sound/tom.wav",
+      audio: "./Hafta-6/Odev_2_DrumKit/assests/sound/tom.wav",
     },
   ];
 
