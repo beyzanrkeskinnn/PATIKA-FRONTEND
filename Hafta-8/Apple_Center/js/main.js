7030d5d4671a4e2567341b0ec53299dfb44b7d3c
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-07-28 22:51:33 +0300

    additions were made

---
+++ Hafta-8/Apple_Center/js/main.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+  
+ 
+
+  
+    var camera = new Swiper('#camera .swiper', {
+      speed: 600,
+      spaceBetween: 12,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+
+    var glightbox = GLightbox({
+      selector: '.glightbox',
+    })
+    
+})();
